94648a709c4cbe30b2bb95ddd082de49abead9c7
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-02-24 12:22:17 +0400

    Первый коммит

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let answer = prompt("Какое «официальное» название JavaScript?");
+
+if (answer === "ECMAScript") {
+  alert("Верно!");
+} else {
+  alert("Не знаете? ECMAScript!");
+}
