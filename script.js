6fbdaab84f5f5705822105659c43dc0701377af4
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-02-26 18:33:08 +0400

    Напишите код, который выводит все простые числа из интервала от 2 до n.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let i = prompt("введите число, больше 100");
-while (i < 100) {
-  i = prompt("Введите ещё раз");
-}
+let n = 10;
 
-alert("Готово!");
+for (let i = 2; i <= n; i++) {
+  let prostoe = true;
+
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) prostoe = false;
+  }
+
+  if (prostoe) console.log(i);
+}
