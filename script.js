419b4b38e48e54ea5e7f0b451740fe9ae18f14b7
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-02-27 15:13:53 +0400

    Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b.

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-i = browser;
-if (i === "Edge") {
-  alert("You've got the Edge!");
-} else if (
-  i === "Chrome" ||
-  i === "Firefox" ||
-  i === "Safari" ||
-  i === "Opera"
-) {
-  alert("Okay we support these browsers too");
-} else {
-  alert("We hope that this page looks ok!");
+function min(a, b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
 }
