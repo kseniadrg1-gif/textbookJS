c07d860a18d3b1788961348aaa78527e1a41c2d4
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-02-28 13:48:18 +0400

    Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let user = {
-  name: "John",
-  surname: "Smith",
-};
-user.name = "Pete";
-delete user.name;
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
