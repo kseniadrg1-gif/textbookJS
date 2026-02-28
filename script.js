4b02a5f69a3e6d7d04e5317b4e9233054f2e066d
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-02-28 16:54:10 +0400

    Создайте объект calculator (калькулятор) с тремя методами:
    read() (читать) запрашивает два значения и сохраняет их как свойства объекта с именами a и b.
    sum() (суммировать) возвращает сумму сохранённых значений.
    mul() (умножить) перемножает сохранённые значения и возвращает результат.
    let calculator = {
      // ... ваш код ...
    };
    
    calculator.read();
    alert( calculator.sum() );
    alert( calculator.mul() );

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function isEmpty(obj) {
-  for (let key in obj) {
-    return false;
-  }
-  return true;
-}
+let calculator = {
+  a: 0,
+  b: 0,
+
+  read() {
+    this.a = +prompt("Введите первое значение:", 0);
+    this.b = +prompt("Введите второе значение:", 0);
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
