87e722268044b1f8e76fedfaa8f511dc58e9b10d
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-02-28 13:38:25 +0400

    Напишите код, выполнив задание из каждого пункта отдельной строкой:
    Создайте пустой объект user.
    Добавьте свойство name со значением John.
    Добавьте свойство surname со значением Smith.
    Измените значение свойства name на Pete.
    Удалите свойство name из объекта.

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-function ask(question, yes, no) {
-  if (confirm(question)) yes();
-  else no();
-}
-
-ask(
-  "Вы согласны?",
-  () => alert("Вы согласились."),
-  () => alert("Вы отменили выполнение."),
-);
+let user = {
+  name: "John",
+  surname: "Smith",
+};
+user.name = "Pete";
+delete user.name;
