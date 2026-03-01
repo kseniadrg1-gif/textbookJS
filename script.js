35e12c11404bd41077555b13591d1b7df73fb2e9
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-01 20:14:40 +0400

    Создайте скрипт, который запрашивает ввод двух чисел (используйте prompt) и после показывает их сумму.

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-let calculator = {
-  a: 0,
-  b: 0,
-
-  read() {
-    this.a = +prompt("Введите первое значение:", 0);
-    this.b = +prompt("Введите второе значение:", 0);
-  },
-
-  sum() {
-    return this.a + this.b;
-  },
-
-  mul() {
-    return this.a * this.b;
-  },
-};
-
-calculator.read();
-alert(calculator.sum());
-alert(calculator.mul());
+let a = +prompt("Введите 1 число");
+let b = +prompt("Введите 2 число");
+alert(a + b);
