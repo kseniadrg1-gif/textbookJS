b5b67ed0ac121a1e391c1ea3dc2ecd693d75bd14
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-02 18:51:31 +0400

    Давайте произведём 5 операций с массивом.
    Создайте массив styles с элементами «Джаз» и «Блюз».
    Добавьте «Рок-н-ролл» в конец.
    Замените значение в середине на «Классика». Ваш код для поиска значения в середине должен работать для массивов с любой длиной.
    Удалите первый элемент массива и покажите его.
    Вставьте Рэп и Регги в начало массива.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function checkSpam(str) {
-  return (
-    str.toLowerCase().includes("viagra") || str.toLowerCase().includes("xxx")
-  );
-}
+let styles = ["Джаз", "Блюз"];
+
+console.log(styles);
+
+styles.push("Рок-н-ролл");
+
+console.log(styles);
+
+let middleIndex = Math.floor(styles.length / 2);
+styles[middleIndex] = "Классика";
+
+console.log(styles);
+
+let firstElement = styles.shift();
+console.log(firstElement);
+
+console.log(styles);
+
+styles.unshift("Рэп", "Регги");
+console.log(styles);
