43d7ae3fd570abf103bae0800445b44b8c6075c9
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-02 22:51:36 +0400

    Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
    То есть дефисы удаляются, а все слова после них получают заглавную букву.

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-function sumInput() {
-  let numbers = []; // Создаем пустой массив для чисел
-
-  while (true) {
-    // Бесконечный цикл
-    let value = prompt("Введите число", 0); // Запрашиваем значение
-
-    // Проверяем, нужно ли остановить ввод
-    if (value === null || value === "" || isNaN(value)) {
-      break; // Выходим из цикла
-    }
-
-    numbers.push(Number(value)); // Преобразуем в число и добавляем в массив
-  }
-
-  // Подсчитываем сумму
-  let sum = 0;
-  for (let num of numbers) {
-    sum += num;
-  }
-
-  return sum; // Возвращаем сумму
+function camelize(str) {
+  return str
+    .split("-")
+    .map((word, index) => {
+      if (index === 0) {
+        return word;
+      } else {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }
+    })
+    .join("");
 }
-
-// Вызов функции
-let result = sumInput();
-alert("Сумма: " + result);
