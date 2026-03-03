121c4adb4a107d4bdbc0e2fbb80daf1c40301afe
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-03 19:09:00 +0400

    Создайте функцию unique(arr), которая вернёт массив уникальных, не повторяющихся значений массива arr.

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function camelize(str) {
-  return str
-    .split("-")
-    .map((word, index) => {
-      if (index === 0) {
-        return word;
-      } else {
-        return word.charAt(0).toUpperCase() + word.slice(1);
-      }
-    })
-    .join("");
+function unique(arr) {
+  return Array.from(new Set(arr));
 }
