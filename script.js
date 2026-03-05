2256bc6d7c61099409ad62653d49f5379c7dca18
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-05 18:56:55 +0400

    Дано число. Выведите количество цифр в этом числе.

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-let number = prompt("Введи число, узнай его первую и последнюю цифру");
-let firstDigit = number.at(0);
-let lastDigit = number.at(-1);
-let sum = Number(firstDigit) + Number(lastDigit);
-alert(sum);
+let number = prompt("Введи число");
+alert(number.length);
