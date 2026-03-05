96695ffb2af9b648d2dccb7bac7b3eb817d66af3
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-05 18:55:19 +0400

    Дано число. Выведите в консоль сумму первой и последней цифры этого числа.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-let number = prompt("Введи число, узнай его последнюю цифру");
-let firstDigit = number.at(-1);
-alert(firstDigit);
+let number = prompt("Введи число, узнай его первую и последнюю цифру");
+let firstDigit = number.at(0);
+let lastDigit = number.at(-1);
+let sum = Number(firstDigit) + Number(lastDigit);
+alert(sum);
