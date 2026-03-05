aeb41ba567b761f11194bdab983e4b9b4095129d
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-05 19:25:57 +0400

    Выведите в консоль все целые числа от 1 до 100.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let number1 = prompt("Введи первое число");
-let number2 = prompt("Введи второе число");
-if (Number(number1) % Number(number2) === 0) {
-  alert("Первое число без остатка делится на второе");
-} else {
-  alert("Первое число с остатком делится на второе");
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
 }
