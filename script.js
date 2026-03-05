41d778e7206e4b201214f9a9c4ae6f5c9b8ac231
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-05 19:08:08 +0400

    Дана строка. Если в этой строке более одного символа, выведите в консоль предпоследний символ этой строки.

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-let number = prompt("Введи первое число");
-let number2 = prompt("Введи второе число");
-firstDigitNnmber = number.at(0);
-firstDigitNnmber2 = number2.at(0);
-if (firstDigitNnmber === firstDigitNnmber2) {
-  alert("Первые цифры этих чисел совпадают");
-} else {
-  alert("Первые цифры этих чисел не совпадают");
+let text = prompt("Введи строку");
+if (text.length >= 2) {
+  alert(text.at(-2));
 }
