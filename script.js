24d8a8c701d82f8b890dc24441b191890a1cc06f
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-05 19:14:38 +0400

    Даны два целых числа. Проверьте, что первое число без остатка делится на второе.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let text = prompt("Введи строку");
-if (text.length >= 2) {
-  alert(text.at(-2));
+let number1 = prompt("Введи первое число");
+let number2 = prompt("Введи второе число");
+if (Number(number1) % Number(number2) === 0) {
+  alert("Первое число без остатка делится на второе");
+} else {
+  alert("Первое число с остатком делится на второе");
 }
