ad2920bbea7f3ddd3995bcdf386dcee607ea4e36
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-05 18:25:11 +0400

    Дана строка. Выведите в консоль длину этой строки.

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
-let room = {
-  number: 23,
-};
-
-let meetup = {
-  title: "Совещание",
-  occupiedBy: [{ name: "Иванов" }, { name: "Петров" }],
-  place: room,
-};
-
-room.occupiedBy = meetup;
-meetup.self = meetup;
-
-alert(
-  JSON.stringify(meetup, function replacer(key, value) {
-    return key != "" && value == meetup ? undefined : value;
-  }),
-);
+let text = prompt("Введи строку");
+alert(text.length);
