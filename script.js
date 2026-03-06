2211482da14074c7f156861d1d0a42de054e0773
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-06 13:19:48 +0400

    Дан массив с числами. Найдите сумму тех элементов этого массива, которые больше нуля и меньше десяти.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let arr = [5, 6, 7, 8];
+let arr = [5, 6, 7, 8, 11, 12];
 let sum = 0;
 for (i = 0; i < arr.length; i++) {
-  if (arr[i] > 0) {
+  if (arr[i] > 0 && arr[i] < 10) {
     sum += arr[i];
   }
 }
