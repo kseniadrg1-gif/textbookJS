0cd95a3aa6b3ac899e860e22913bdba3b8a5617b
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-06 12:02:42 +0400

    Дан массив с числами. Найдите сумму квадратов элементов этого массива.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let text = prompt("Введите строку");
-for (let i = text.length - 1; i >= 0; i--) {
-  alert(text[i]);
+let arr = [5, 6, 7, 8];
+let sum = 0;
+for (i = 0; i < arr.length; i++) {
+  sum = sum + arr[i] * arr[i];
 }
+console.log(sum);
