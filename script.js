efb6529eefeabb1ed37ae5441f8c78a7d770dbe7
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-06 13:14:02 +0400

    Дан массив с числами. Найдите сумму положительных элементов этого массива.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let arr = [5, 6, 7, 8];
 let sum = 0;
-for (i = 0; i < arr.length; i++) sum = sum + Math.sqrt(arr[i]);
-alert(sum);
+for (i = 0; i < arr.length; i++) {
+  if (arr[i] > 0) {
+    sum += arr[i];
+  }
+}
+console.log(sum);
