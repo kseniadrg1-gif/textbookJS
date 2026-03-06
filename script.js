5f5bbf8d763c8da30ef8f0c9bc382dfdc51a8ddb
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-06 13:27:40 +0400

    Дана строка:
    'abcde'
    Получите массив букв этой строки.

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let arr = [5, 6, 7, 8, 11, 12];
-let sum = 0;
-for (i = 0; i < arr.length; i++) {
-  if (arr[i] > 0 && arr[i] < 10) {
-    sum += arr[i];
-  }
-}
-console.log(sum);
+let str = "abcde";
+let arr = str.split("");
+alert(arr);
