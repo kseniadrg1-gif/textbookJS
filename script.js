ce63c775ccddd4db6f8db61d2c417a8aad4fd176
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-06 13:06:06 +0400

    Дан массив с числами. Найдите сумму квадратных корней элементов этого массива.

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 let arr = [5, 6, 7, 8];
 let sum = 0;
-for (i = 0; i < arr.length; i++) {
-  sum = sum + arr[i] * arr[i];
-}
-console.log(sum);
+for (i = 0; i < arr.length; i++) sum = sum + Math.sqrt(arr[i]);
+alert(sum);
