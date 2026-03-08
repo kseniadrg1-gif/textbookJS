a570f092f999ce48e2baa77dd0aeaf57de609a56
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-08 22:19:40 +0400

    Измените код makeCounter() так, чтобы счётчик мог уменьшать и устанавливать значение:
    counter() должен возвращать следующее значение (как и раньше).
    counter.set(value) должен устанавливать счётчику значение value.
    counter.decrease() должен уменьшать значение счётчика на 1.
    Посмотрите код из песочницы с полным примером использования.
    
    P.S. Для того, чтобы сохранить текущее значение счётчика, можно воспользоваться как замыканием, так и свойством функции. Или сделать два варианта решения: и так, и так.
    
    Открыть песочницу с тестами для задачи.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-arr = [1.456, 2.125, 3.32, 4.1, 5.34];
-let rounded = arr.map((num) => Math.round(num * 10) / 10);
-console.log(rounded);
+function makeCounter() {
+  let count = 0;
+
+  function counter() {
+    return count++;
+  }
+
+  counter.set = (value) => (count = value);
+
+  counter.decrease = () => count--;
+
+  return counter;
+}
