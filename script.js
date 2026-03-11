4aaeae053d0eb17f11d073701dfeda6ebe26a5ba
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-11 17:57:28 +0400

    Напишите функцию printNumbers(from, to), которая выводит число каждую секунду, начиная от from и заканчивая to.
    Сделайте два варианта решения.
    
    Используя setInterval.
    Используя рекурсивный setTimeout.

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-function makeCounter() {
-  let count = 0;
-
-  function counter() {
-    return count++;
-  }
-
-  counter.set = (value) => (count = value);
-
-  counter.decrease = () => count--;
-
-  return counter;
+function printNumbers(from, to) {
+  let current = from;
+  let timerId = setInterval(function () {
+    console.log(current);
+    if (current == to) {
+      clearInterval(timerId);
+    }
+    current++;
+  }, 1000);
 }
