b1df67536bf3718e0e9ace72848b338668133dbe
kseniadrg1-gif <kseniadrg1@gmail.com>
2026-03-11 18:04:21 +0400

    Создайте декоратор spy(func), который должен возвращать обёртку, которая сохраняет все вызовы функции в своём свойстве calls.
    Каждый вызов должен сохраняться как массив аргументов.

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-function printNumbers(from, to) {
-  let current = from;
-  let timerId = setInterval(function () {
-    console.log(current);
-    if (current == to) {
-      clearInterval(timerId);
-    }
-    current++;
-  }, 1000);
+function spy(func) {
+  function wrapper(...args) {
+    wrapper.calls.push(args);
+    return func.apply(this, args);
+  }
+  wrapper.calls = [];
+  return wrapper;
 }
